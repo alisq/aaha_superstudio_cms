68c5435ef70e295318d72334b6037d595c19cd5a
alisq <ali.shamas.qadeer@gmail.com>
2026-01-02 10:49:37 -0500

    pusha

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const base = process.env.PUBLIC_URL;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={base}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
+
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
