c2371079b4f486b77b30d763364cdee47ee58b11
alisq <ali.shamas.qadeer@gmail.com>
2026-01-02 10:43:14 -0500

    s

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const base = process.env.PUBLIC_URL;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={base}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
