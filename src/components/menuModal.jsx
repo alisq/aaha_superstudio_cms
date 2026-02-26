651b883e2c141c1046b12fada3fbf6010a4b5704
alisq <ali.shamas.qadeer@gmail.com>
2026-02-26 10:21:55 -0500

    all images

--- src/components/menuModal.jsx
+++ src/components/menuModal.jsx
@@ -1,9 +1,17 @@
 function MenuModal () {
 
 return (
-    <>
-    <link to="#about"><h5>about the superstudio</h5></link>
-    </>
+    <menu id="overlay">
+        <ul>
+            <li>
+                <link to="#about"><h5>about the superstudio</h5></link>
+            </li>
+        
+            <li>
+                <link to="/"><h5>student work</h5></link>
+            </li>
+        </ul>
+    </menu>
     
 )
 
