920b19cb58070389251b6b28f3a0292463cb2bd4
alisq <ali.shamas.qadeer@gmail.com>
2026-04-23 15:58:45 -0400

    added thumbnails

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,22 @@
+
+import { Link } from "react-router-dom";
+
 function Footer() {
     return (
         <footer>
-            
+            <ul className="menu">
+          <li>
+            <Link to="/about">
+              about the superstudio
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/">
+              student work
+            </Link>
+          </li>
+        </ul>
         </footer>
     )
 }
